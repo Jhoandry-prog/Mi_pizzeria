d51125bbbcc2ac90283ac1e776e90b38e1a624c2
Jhoandry carrasquero <jhoandrycarrasquero078@gmail.com>
2024-12-15 14:31:36 -0300

    se hace ajuste de texto en header

--- src/componente/Header.jsx
+++ src/componente/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
       <header className="text-center my-4">
-        <h1>¡Pizzería Mamma Mia!</h1>
+        <h1>¡Pizzería Jhoa!</h1>
         <p>Tenemos las mejores pizzas que podrás encontrar.</p>
       </header>
     );
