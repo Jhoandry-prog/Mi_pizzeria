d63732fbd1b801c46042a398b8cc632a2d77aba6
Jhoandry carrasquero <jhoandrycarrasquero078@gmail.com>
2025-02-02 23:44:21 -0300

    se guardan cambios del archivo Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,40 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react';
 import Header from '../components/Header';
 import CardPizza from '../components/CardPizza';
+import { CartContext } from '../context/cartContext';
 
 const Home = () => {
-  const [menu,setPizzas] = useState([])
+  const [menu, setMenu] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { addToCart } = useContext(CartContext);
 
   const getMenu = async () => {
-    const response = await fetch("http://localhost:5000/api/pizzas");
-    const data = await response.json();
-    setMenu(data);
-    setIsLoading(false);
+    try {
+      const response = await fetch("http://localhost:5000/api/pizzas");
+      const data = await response.json();
+      setMenu(data);
+    } catch (error) {
+      console.error("Error al obtener las pizzas:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-      getPizzas()
-  }, [])
-
+    getMenu();
+  }, []);
 
   return (
     <>
-      
-      <Header/>
+      <Header />
 
       <main>
-        {isLoading ? (<p>Cargando...</p>) : (menu.map(pizza => <CardPizza
-          name={pizza.name}
-          price={pizza.price.toLocaleString('de-DE')}
-          ingredients={pizza.ingredients}
-          img={`public/imgs/${pizza.name}.jpeg`}
-          key={pizza.id}
-        />))}
-
+        {isLoading ? (
+          <p>Cargando...</p>
+        ) : (
+          menu.map(pizza => (
+            <CardPizza
+              key={pizza.id}
+              name={pizza.name}
+              price={pizza.price.toLocaleString('de-DE')}
+              ingredients={pizza.ingredients}
+              img={`/public/imgs/${pizza.name}.jpeg`}
+              onAddToCart={() => addToCart(pizza)} // Pasar función de "Añadir"
+            />
+          ))
+        )}
       </main>
     </>
-  )
-}
+  );
+};
 
 export default Home;
